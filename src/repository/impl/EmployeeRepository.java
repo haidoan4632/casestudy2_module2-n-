bd5ef8935cf2204a386f4c8857684f617cf708c4
haidoan4632 <doanhai01287181543@gmail.com>
2023-03-14 21:51:47 +0700

    first commit

--- src/repository/impl/EmployeeRepository.java
+++ src/repository/impl/EmployeeRepository.java
@@ -1,14 +1,13 @@
 package repository.impl;
 
 import common.ReadFile;
-import common.WriteFile;
 import model.person.Employee;
 import repository.IEmployeeRepository;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static common.WriteFile.*;
+import static common.WriteFile.write;
 
 public class EmployeeRepository implements IEmployeeRepository {
     public static final String EMPLOYEE_PATH = "src\\data\\employee.csv";
@@ -25,7 +24,6 @@ public class EmployeeRepository implements IEmployeeRepository {
             employeeList.add(employee);
         }
         return employeeList;
-
     }
 
     @Override
@@ -67,12 +65,33 @@ public class EmployeeRepository implements IEmployeeRepository {
     @Override
     public void edit(Employee employee) {
         List<Employee> result = getALl();
-        for (int i = 0; i <result.size();i++){
-            if (result.get(i).getEmployeeCode().equals(employee.getEmployeeCode())){
-                result.set(i,employee);
+        for (int i = 0; i < result.size(); i++) {
+            if (result.get(i).getEmployeeCode().equals(employee.getEmployeeCode())) {
+                result.set(i, employee);
             }
         }
         List<String> stringList = new ArrayList<>();
-        System.out.println(result);  
+        String[] temp = new String[10];
+        String string;
+        for (Employee employee1 : result) {
+            temp[0] = employee1.getName();
+            temp[1] = employee1.getDateOfBirth();
+            temp[2] = employee1.getGender();
+            temp[3] = employee1.getIdCard();
+            temp[4] = employee1.getPhoneNumber();
+            temp[5] = employee1.getEmail();
+            temp[6] = employee1.getEmployeeCode();
+            temp[7] = employee1.getDegree();
+            temp[8] = employee1.getPosition();
+            temp[9] = employee1.getSalary();
+            string = new String(temp[0] + "," + temp[1] + "," + temp[2] + "," + temp[3] + "," + temp[4] + "," + temp[5] + "," + temp[6] + "," + temp[7] + "," + temp[8] + "," + temp[9]);
+            stringList.add(string);
+            try {
+                write("src\\data\\employee.csv", stringList);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 }
